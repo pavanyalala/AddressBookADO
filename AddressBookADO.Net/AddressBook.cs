599fd6c1284b22fb92c4cb241d11c54c2eca51cf
pavanyalala <pavanyalala4508@gmail.com>
2021-09-08 23:45:09 +0530

    [pavan]:connected to data base server

---
+++ AddressBookADO.Net/AddressBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBookADO.Net
+{
+    public class AddressBook
+    {
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AddressBookServiceADO";
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}
