a6bb2a77c662067650f7be790cc537364f83f404
pavanyalala <pavanyalala4508@gmail.com>
2021-09-08 23:49:41 +0530

    [pavan]:Table creation is done in data base

---
+++ AddressBookADO.Net/ContactDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookADO.Net
+{
+    public class ContactDetails
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public double PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
--- AddressBookAdo.sql
+++ AddressBookAdo.sql
@@ -1,1 +1,13 @@
 create database AddressBookADO
+
+create table AddressBook
+(
+  FirstName varchar(20) not null,
+  LastName varchar(20) not null,
+  Address varchar(20) not null,
+  City varchar(20) not null,
+  State varchar(20) not null,
+  Zip float not null,
+  PhoneNumber float not null,
+  Email varchar(50) not null
+)
