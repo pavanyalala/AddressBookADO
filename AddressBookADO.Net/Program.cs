6bb5de77902974f4bf0d6257b994c2f30f75d105
pavanyalala <pavanyalala4508@gmail.com>
2021-09-08 23:38:17 +0530

    [pavan]:Initial message added

---
+++ AddressBookADO.Net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookADO.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook ADO.Net");
+        }
+    }
+}
